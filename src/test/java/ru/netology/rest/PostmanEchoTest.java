7689c6f3045e89f61e40f4bfb26bba4c7acc443a
dryworts <omzg@mail.ru>
2023-10-24 23:11:42 +0600

    improved tests

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hi,bro"));
+                .body("data", equalTo("Hi"));
     }
 
 
